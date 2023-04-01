7872cfc1b4234e7a4b91f232e8f04633865170b1
Edilson da Sara <118057467+Ed-JackBoy-B7@users.noreply.github.com>
2023-04-01 03:07:46 -0700

    Conta Banco

--- ContaBanco/src/contabanco/ContaTerminal.java
+++ ContaBanco/src/contabanco/ContaTerminal.java
@@ -6,15 +6,35 @@ package contabanco;
 
 /**
  *
- * @author MASSINGUE
+ * @author Edilson Da Sara
  */
-public class ContaBanco {
+import java.util.Scanner;
+public class ContaTerminal {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //TODO: Conhecer e importara aclasse scanner;
+        //Exibir mensagens para usuarios;
+        //Obter pela classe scanner os valores digitados no terminal
+        //Exibir a mensagem conta criada
+        
+        Scanner leitura= new Scanner (System.in);
+        System.out.println("Digite o seu nome:");
+        String nomeCliente=leitura.nextLine();
+        
+        System.out.println("Digite o seu nome Agencia:");
+        String agencia=leitura.nextLine();
+        
+        System.out.println("Por favor, digite o numero da Agencia:");
+          int numero=leitura.nextInt();
+          
+          System.out.println("Digite o seu saldo:");
+            double saldo=leitura.nextDouble();
+          
+          System.out.println("Ola "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agencia e' "+agencia+" conta "+numero+" e seu saldo "+saldo+" ja esta disponivel para saque.");
     }
     
 }
